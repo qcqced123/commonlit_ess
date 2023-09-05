3dfefa5895be43163aea84fb490fca78fe376667
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-09-05 16:12:51 +0900

    Update dataclass.py

--- dataset_class/dataclass.py
+++ dataset_class/dataclass.py
@@ -10,6 +10,59 @@ from dataset_class.preprocessing import subsequent_tokenizing, subsequent_decode
 from trainer.trainer_utils import SmartBatchingSampler, SmartBatchingCollate
 
 
+class OneToOneDataset(Dataset):
+    """
+    For Supervised Learning Pipeline, making "type 1" prompt sentence for LLMs Inputs
+    This class have 2 functions
+        1) apply preprocessing for mis-spelling words in prompt texts & summaries texts (maybe later applying)
+        2) make prompt sentence with no padding (x)
+        => this pipeline will be set hyper-params 'max_len' as at least 1920,
+        which is maximum value of all instance's prompt sentence length
+    Args:
+        cfg: config module from configuration.py
+        p_df: prompt train dataset, prompts_train.csv
+        s_df: prompt train dataset, summaries_train.csv
+    """
+    def __init__(self, cfg: configuration.CFG, p_df: pd.DataFrame, s_df: pd.DataFrame) -> None:
+        self.cfg = cfg
+        self.tokenizing = tokenizing
+        self.s_df = s_df
+        self.p_ids = p_df.prompt_id.to_numpy()  # which is connection key of prompt & summaries
+        self.p_titles = p_df.prompt_title.to_numpy()
+        self.p_texts = p_df.prompt_text.to_numpy()
+        self.p_questions = p_df.prompt_question.to_numpy()
+        self.s_ids = s_df.prompt_id.to_numpy()  # which is connection key of prompt & summaries
+        self.s_texts = s_df.text.to_numpy()
+
+    def __len__(self) -> int:
+        return len(self.s_ids)
+
+    def __getitem__(self, item: int) -> tuple[dict, Tensor]:
+        # 1) load special token for making prompt sentence
+        cls, sep = self.cfg.tokenizer.cls_token, self.cfg.tokenizer.sep_token
+        com, tar = self.cfg.tokenizer.common_token, self.cfg.tokenizer.tar_token
+
+        # 2) load feature for making prompt sentence & LLM's inputs
+        key = find_index(self.p_ids, self.s_ids[item])
+        """
+        3) make prompt sentence for LLM's inputs
+            - summaries_text + prompt_question + prompt_title + prompt_text => 0
+            - summaries_text + prompt_question + prompt_title
+            - summaries_text + prompt_question + prompt_text
+            - summaries_text + prompt_title + prompt_text
+        """
+        prompt = cls + tar + self.s_texts[item] + tar + sep
+#        prompt += com + self.p_questions[key] + com + self.p_titles[key] + com + self.p_texts[key] + com + sep
+        prompt += com + self.p_questions[key] + com + self.p_titles[key] + com + sep
+
+        inputs = self.tokenizing(self.cfg, prompt)
+        labels = torch.as_tensor(self.s_df.iloc[item, 3:5], dtype=torch.float)
+
+        del prompt
+        gc.collect()
+        return inputs, labels
+
+
 class OneToOneSmartBatchDataset(Dataset):
     """
     For Supervised Learning Pipeline, making "type 1" prompt sentence for LLMs Inputs
@@ -70,59 +123,6 @@ class OneToOneSmartBatchDataset(Dataset):
             drop_last=drop_last
         )
         return smart_dataloader
-
-
-class OneToOneDataset(Dataset):
-    """
-    For Supervised Learning Pipeline, making "type 1" prompt sentence for LLMs Inputs
-    This class have 2 functions
-        1) apply preprocessing for mis-spelling words in prompt texts & summaries texts (maybe later applying)
-        2) make prompt sentence with no padding (x)
-        => this pipeline will be set hyper-params 'max_len' as at least 1920,
-        which is maximum value of all instance's prompt sentence length
-    Args:
-        cfg: config module from configuration.py
-        p_df: prompt train dataset, prompts_train.csv
-        s_df: prompt train dataset, summaries_train.csv
-    """
-    def __init__(self, cfg: configuration.CFG, p_df: pd.DataFrame, s_df: pd.DataFrame) -> None:
-        self.cfg = cfg
-        self.tokenizing = tokenizing
-        self.s_df = s_df
-        self.p_ids = p_df.prompt_id.to_numpy()  # which is connection key of prompt & summaries
-        self.p_titles = p_df.prompt_title.to_numpy()
-        self.p_texts = p_df.prompt_text.to_numpy()
-        self.p_questions = p_df.prompt_question.to_numpy()
-        self.s_ids = s_df.prompt_id.to_numpy()  # which is connection key of prompt & summaries
-        self.s_texts = s_df.text.to_numpy()
-
-    def __len__(self) -> int:
-        return len(self.s_ids)
-
-    def __getitem__(self, item: int) -> tuple[dict, Tensor]:
-        # 1) load special token for making prompt sentence
-        cls, sep = self.cfg.tokenizer.cls_token, self.cfg.tokenizer.sep_token
-        com, tar = self.cfg.tokenizer.common_token, self.cfg.tokenizer.tar_token
-
-        # 2) load feature for making prompt sentence & LLM's inputs
-        key = find_index(self.p_ids, self.s_ids[item])
-        """
-        3) make prompt sentence for LLM's inputs
-            - summaries_text + prompt_question + prompt_title + prompt_text => 0
-            - summaries_text + prompt_question + prompt_title
-            - summaries_text + prompt_question + prompt_text
-            - summaries_text + prompt_title + prompt_text
-        """
-        prompt = cls + tar + self.s_texts[item] + tar + sep
-#        prompt += com + self.p_questions[key] + com + self.p_titles[key] + com + self.p_texts[key] + com + sep
-        prompt += com + self.p_questions[key] + com + self.p_titles[key] + com + sep
-
-        inputs = self.tokenizing(self.cfg, prompt)
-        labels = torch.as_tensor(self.s_df.iloc[item, 3:5], dtype=torch.float)
-
-        del prompt
-        gc.collect()
-        return inputs, labels
 
 
 class OneToManyDataset(Dataset):
