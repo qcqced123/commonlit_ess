5ad8e868b31ffc8ababe79995c43ad7fff37419d
qcqced123 <97833081+qcqced123@users.noreply.github.com>
2023-11-12 18:37:49 +0900

    Update train.py

--- train.py
+++ train.py
@@ -25,20 +25,5 @@ def main(config_path: str, cfg: CFG) -> None:
 
 
 if __name__ == '__main__':
-    # args = argparse.ArgumentParser(description='PyTorch Template')
-    # args.add_argument('-c', '--config', default=None, type=str,
-    #                   help='config file path (default: None)')
-    # args.add_argument('-r', '--resume', default=None, type=str,
-    #                   help='path to latest checkpoint (default: None)')
-    # args.add_argument('-d', '--device', default=None, type=str,
-    #                   help='indices of GPUs to enable (default: all)')
-    #
-    # # custom cli options to modify configuration from default values given in json file.
-    # CustomArgs = collections.namedtuple('CustomArgs', 'flags type target')
-    # options = [
-    #     CustomArgs(['--lr', '--learning_rate'], type=float, target='optimizer;args;lr'),
-    #     CustomArgs(['--bs', '--batch_size'], type=int, target='dataset_class;args;batch_size')
-    # ]
-    # cli_config = ConfigParser.from_args(args, options)
     main('one2one_train_cfg.json', CFG)
 
